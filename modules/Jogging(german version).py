77b19ebcb9297fd2350bb23c07e66523bbd2210a
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-11-11 13:59:19 +0100

    Add files via upload

--- modules/Jogging(german version).py
+++ modules/Jogging(german version).py
@@ -7,6 +7,7 @@ Created on Tue Nov  3 15:35:42 2020
 the default language of my computer is german....cry!
 """
 
+import os
 import speech
 import time
 import numpy as np
@@ -27,9 +28,41 @@ class Debug:
                         "Neu Minuten vergingen", "Zehn Minuten vergingen", "Elf Minuten vergingen", "Zwölf Minuten vergingen",
                         "dreizehn Minuten vergingen", "vierzehn Minuten vergingen", "fünfzehn Minuten vergingen", "Sechszehn Minuten vergingen",
                         "Siebzehn Minuten vergingen", "Achtzehn Minuten vergingen", "Neunzehn Minuten vergingen", "Zwanzig Minuten vergingen"]
-        
+
+        if not os.path.exists("./time_data_jogging.txt"):
+            local_time = time.localtime()
+            self.time_list = [time.strftime("%Y", local_time), time.strftime("%m", local_time),
+                              time.strftime("%d", local_time),
+                              time.strftime("%H", local_time), time.strftime("%M", local_time),
+                              time.strftime("%S", local_time)]
+        else:
+            with open("./time_data_jogging.txt", "r") as f:
+                time_data_list = f.readlines()
+                new_data_list = []
+                for data in time_data_list:
+                    new_data_list.append(int(data.rstrip()))
+                self.time_list = new_data_list
+            # we need to judge here, whether the time gap reaches 6 hours
+            # more than two days
+            year_difference = int(time.strftime("%Y")) - self.time_list[0]
+            month_difference = int(time.strftime("%m")) - self.time_list[1]
+            day_difference = int(time.strftime("%d")) - self.time_list[2]
+            hour_difference = int(time.strftime("%H")) - self.time_list[3]
+            # the same day
+            if year_difference == 0 and month_difference == 0 and day_difference == 0 and hour_difference < 6:
+                print("两次运动时间间隔不足六个小时，请保证充足地休息！")
+                print("")
+                self.flag = False
+            # the different day
+            if year_difference == 0 and month_difference == 0 and day_difference == 1 and hour_difference < -18:
+                print("两次运动时间间隔不足六个小时，请保证充足地休息！")
+                print("")
+                self.flag = False
+
     def main_program(self):
-        self.remind_message()
+        if self.flag:
+            self.remind_message()
+            self.update_data()
         
     def remind_message(self):
         speech.say("los geht's")
@@ -42,8 +75,14 @@ class Debug:
                 
         speech.say("Fertig!")
         
-    def gui(self):
-        pass
+    def update_data(self):
+        with open("./time_data_jogging.txt", "w+") as f:
+            f.write(str(self.time_list[0]) + '\n')
+            f.write(str(self.time_list[1]) + '\n')
+            f.write(str(self.time_list[2]) + '\n')
+            f.write(str(self.time_list[3]) + '\n')
+            f.write(str(self.time_list[4]) + '\n')
+            f.write(str(self.time_list[5]) + '\n')
     
 
 if __name__ == "__main__":
